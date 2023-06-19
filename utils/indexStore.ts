54369ed85ba0dc36a3f6ee36b593afbe19199058
t-mc4 <business.basics@aleeas.com>
2023-06-18 21:53:09 -0700

    Refactored all prompts/ai-completions to new versions

--- utils/indexStore.ts
+++ utils/indexStore.ts
@@ -2,60 +2,128 @@ import { OpenAI } from "../utils/openAI";
 import dotenv from "dotenv";
 import fetch from "node-fetch";
 import { getIndexNamePrompt, getPriorityDecisionPrompt, getAnswerPrompt } from "./prompts";
-import jsonData from '../scripts/docs.json'
+import jsonData from "../scripts/docs.json";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { LLMChain, ConversationChain } from "langchain/chains";
+import { BufferMemory } from "langchain/memory";
 
 dotenv.config();
 const key = process.env.OPENAI_API_KEY || "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";
 const model = process.env.OPENAI_MODEL || "gpt-3.5-turbo";
-const inMemorySearchURL = process.env.IN_MEMORY_SEARCH_URL || "https://sherlock-inmemorysearch-production.up.railway.app";
+const inMemorySearchURL =
+  process.env.IN_MEMORY_SEARCH_URL || "https://sherlock-inmemorysearch-production.up.railway.app";
 
 export const openAI = new OpenAI(key);
 
 export async function getIndexName(question: string): Promise<string | undefined> {
+  // Creating a new instance of the OpenAI class and passing in the OPENAI_KEY environment variable
+  let sanitizedQuestion: string = question.trim().replace(/\n/g, " ");
 
-    // Creating a new instance of the OpenAI class and passing in the OPENAI_KEY environment variable
-    let sanitizedQuestion: string = question.trim().replace(/\n/g, " ");;
+  // Use the generateText method to generate text from the OpenAI API and passing the generated prompt, the model, and max token value
+  const chain = new LLMChain({
+    prompt: getIndexNamePrompt(jsonData),
+    llm: new ChatOpenAI({
+      openAIApiKey: key,
+      modelName: "gpt-4",
+      temperature: 0,
+      maxTokens: 2048,
+      streaming: true,
+      callbacks: [
+        {
+          handleLLMNewToken(token) {
+            process.stdout.write(token);
+          },
+        },
+      ],
+    }),
+  });
 
-    // Use the generateText method to generate text from the OpenAI API and passing the generated prompt, the model, and max token value
-    const ans = await openAI.generateText(
-        getIndexNamePrompt(jsonData, sanitizedQuestion),
-        model, 800, 0);
+  const ans = await chain.call({
+    question: sanitizedQuestion,
+  });
 
-    const regex = /(?<=Action:\s)[\w-]+/;
-    const match = ans?.match(regex);
-    return match ? match[0] : undefined;
+  const regex = /(?<=Action:\s)[\w-]+/;
+  const match = ans?.match(regex);
+  return match ? match[0] : undefined;
 }
 
-export async function getPriorityDecision(kpiJsonData: Record<string, number>): Promise<{ high_constraint: string, other_constraints: string[] } | undefined> {
-    let prompt = getPriorityDecisionPrompt(kpiJsonData);
-    const ans = await openAI.generateText(
-        prompt,
-        model, 800, 0);
-    if (ans)
-        return JSON.parse(ans);
-    else
-        return undefined;
+export async function getPriorityDecision(
+  kpiJsonData: Record<string, number>
+): Promise<{ high_constraint: string; other_constraints: string[] } | undefined> {
+  // Create chain and set LLM options
+  const chain = new LLMChain({
+    prompt: getPriorityDecisionPrompt(kpiJsonData),
+    llm: new ChatOpenAI({
+      openAIApiKey: key,
+      modelName: "gpt-4",
+      temperature: 0,
+      maxTokens: 2048,
+      streaming: true,
+      callbacks: [
+        {
+          handleLLMNewToken(token) {
+            process.stdout.write(token);
+          },
+        },
+      ],
+    }),
+  });
+  const ans = await chain.call({
+    input: "Below is the answer you have written based on this while adhering to all the guidelines I gave you:",
+  });
+  if (ans) return JSON.parse(ans.text);
+  else return undefined;
 }
 
 export async function getContextFromIndex(question: string, indexing: string): Promise<any[] | undefined> {
-    let url = inMemorySearchURL + "/api/match";
-    const res = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            sentence: question,
-            indexName: indexing,
-            neighbors: process.env.NN || 1,
-        }),
-    });
-    return res.json();
+  let url = inMemorySearchURL + "/api/match";
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      sentence: question,
+      indexName: indexing,
+      neighbors: process.env.NN || 1,
+    }),
+  });
+  return res.json();
 }
 
-export async function getAnswer(context: any[], question: string, priorityConstraint: string, actionSteps: string): Promise<string | undefined> {
-    // Use the generateText method to generate text from the OpenAI API and passing the generated prompt, the model, and max token value
-    const ans = await openAI.generateText(getAnswerPrompt(context, question, priorityConstraint, actionSteps),
-        model, 800, 0);
-    return ans;
+export async function getAnswer(
+  context: any[],
+  question: string,
+  priorityConstraint: string,
+  actionSteps: string
+): Promise<string | undefined> {
+  // Use the generateText method to generate text from the OpenAI API and passing the generated prompt, the model, and max token value
+  const chain = new ConversationChain({
+    memory: new BufferMemory({
+      returnMessages: true,
+      memoryKey: "history",
+    }),
+    prompt: getAnswerPrompt(context, priorityConstraint, actionSteps),
+    llm: new ChatOpenAI({
+      openAIApiKey: key,
+      modelName: "gpt-3.5-turbo",
+      temperature: 0,
+      maxTokens: 2048, // SET A PRE-DETERMINED LENGTH BEFORE COMPLETION CUTS OFF
+      streaming: true,
+      callbacks: [
+        {
+          handleLLMNewToken(token) {
+            process.stdout.write(token);
+            // dataCallback(token);
+          },
+        },
+      ],
+    }),
+  });
+
+  const ans = await chain.call({
+    input: question,
+  });
+
+  return ans.response;
 }
--- utils/prompts.ts
+++ utils/prompts.ts
@@ -1,33 +1,41 @@
+import {
+  SystemMessagePromptTemplate,
+  HumanMessagePromptTemplate,
+  ChatPromptTemplate,
+  MessagesPlaceholder,
+} from "langchain/prompts";
 
+export function getIndexNamePrompt(jsonData: any[]) {
+  let promptString = "Answer the following questions as best you can. You have access to the following tools:";
+  let indexes = "";
+  for (let index in jsonData) {
+    const { indexName, description } = jsonData[index];
+    promptString += `\n\n${indexName}: ${description}`;
+    indexes += indexName + ",";
+  }
+  indexes = indexes.slice(0, -1);
+  promptString +=
+    `\n\nUse the following format in your response:` +
+    `\nQuestion: the input question you must answer` +
+    `\nThought: you should always think about what to do` +
+    `\nAction: the action to take, should be one of [${indexes}]` +
+    `\nNow respond with the only Action:` +
+    `\nBegin!` +
+    `\nQuestion: {question}` +
+    `\nThought:.`;
 
-export function getIndexNamePrompt(jsonData: any[], question: string) {
-    let promptString =
-        "Answer the following questions as best you can. You have access to the following tools:";
-    let indexes = "";
-    for (let index in jsonData) {
-        const { indexName, description } = jsonData[index];
-        promptString += `\n\n${indexName}: ${description}`;
-        indexes += indexName + ",";
-    }
-    indexes = indexes.slice(0, -1);
-    promptString += (`\n\nUse the following format in your response:` +
-        `\nQuestion: the input question you must answer` +
-        `\nThought: you should always think about what to do` +
-        `\nAction: the action to take, should be one of [${indexes}]` +
-        `\nNow respond with the only Action:` +
-        `\nBegin!` +
-        `\nQuestion: ${question}` +
-        `\nThought:.`);
-
-    return promptString;
+  return ChatPromptTemplate.fromPromptMessages([
+    SystemMessagePromptTemplate.fromTemplate(""),
+    HumanMessagePromptTemplate.fromTemplate(promptString),
+  ]);
 }
 
-export function getPriorityDecisionPrompt(kpiJsonData: Record<string, number>): string {
-    let promptString = `
-    Your job is now to act as my business analyst. I'm going to provide you with a set of current
-    metrics in the business, you are going to evaluate the metrics given with the objective of finding
-    constraints and then identify the #1 focus point based on the order of priority in the list I provide.
+export function getPriorityDecisionPrompt(kpiJsonData: Record<string, number>) {
+  return ChatPromptTemplate.fromPromptMessages([
+    SystemMessagePromptTemplate.fromTemplate(`Your job is now to act as my business analyst. I'm going to provide you with a set of current metrics in the business, you are going to evaluate the metrics given with the objective of finding constraints and then identify the #1 focus point based on the order of priority in the list I provide.
+            
     Each category has a threshold of what is considered a constraint.
+        
     The Metrics You Are Given:
     Contacted Leads - ${kpiJsonData["Contacted Leads"]}
     Responses - ${kpiJsonData["Responses"]}
@@ -46,41 +54,57 @@ export function getPriorityDecisionPrompt(kpiJsonData: Record<string, number>): 
     PiF's - ${kpiJsonData["PIF's"]}
     Pay Plans - ${kpiJsonData["Pay Plans"]}
     Cash Collected - ${kpiJsonData["Cash"]}
+    
     Based on the inputs above, discover constraints based on the following criteria of each
     category:
+    
     Category: Organic Output
     If “responses” is more than 10% of the value given for “contacted leads” AND
     “appointments set” is more than 30% of the value given for “responses” AND “Contacted
     Leads” is less than “250” rule as Constraint
     If “Contacted Leads” is 0, do not evaluate category “Organic Output”
+    
     Category: Organic Response Rate
     If “responses” is less than 10% of the value of “contacted leads” rule as Constraint
+    
     Category: Organic Set Rate-
     If “appointments set” is less than 30% of the value of “responses” rule as Constraint
+    
     Category: CPM's
     If “CPM” is more than $100 rule as Constraint
+    
     Category: CTR
     If “CTR” is less than 1.5% rule as Constraint
+    
     Category: Opt-In Rate
     If “Opt-In Rate” is less than 10% rule as Constraint
+    
     Category: Setter Output
     If “Dials” is less than 200% of “Leads”, rule as Constraint
+    
     Category: Pickup-To-Set Rate
     If “Sets” is less than 10% of “pickups”, rule as Constraint
+    
     Category: DQ Rate
     If “DQ's” is more than 30% of the value of “pickups”, rule as Constraint
+    
     Category: Show Rate
     If “Shows” is less than 70% of the value give for “sets”, rule as constraint
+    
     Category: Close Rate
     If “Closes” is less than 25% of the value given for “shows”, rule as constraint
+    
     Category: PiF Rate
     If “PiF's” is less than 30% of the value given for “closes”, rule as constraint
+    
     Category: Collection Rate
     If “PiF's” PLUS “Pay Plans” is less than 75% of the value given for “closes”, rule as
     constraint
-    Category: Return On Ad Spend-
+    
+    Category: Return On Ad Spend
     If “Cash Collected” is not equal to or greater than the value given for “ad spend”, rule as
     constraint
+    
     Category: Cost Per Lead
     If “Ad Spend” divided by the value of “leads” is more than $30, rule as constraint
     
@@ -100,41 +124,41 @@ export function getPriorityDecisionPrompt(kpiJsonData: Record<string, number>): 
     13. PiF Rate Is Too Low
     14. Collection Rate Is Too Low
     15. Return On Ad Spend Is Too Low
-    Now respond with the following message only customizing the areas in the following format:
+    
+    Respond with the following message only customizing the areas in the following format:
     {
-     "high_constraint": [Your #1 Constraint To Solve Right Now Is Highest Numbered Constraint Identified],
-     "other_constraints": [List Of Other Identified Constraints In Order Of Importance In Array Form]
-    }`;
-
-    return promptString;
+        "high_constraint": [Your #1 Constraint To Solve Right Now Is Highest Numbered Constraint Identified],
+        "other_constraints": [List Of Other Identified Constraints In Order Of Importance In Array Form]
+    }`),
+    HumanMessagePromptTemplate.fromTemplate("{input}"),
+  ]);
 }
 
-export function getAnswerPrompt(context: any[], question: string, priorityConstraint: string, actionSteps: string): string {
+export function getAnswerPrompt(context: any[], priorityConstraint: string, actionSteps: string) {
+  let totalContext = "";
+  // Transfer context from {question,answer} => string
+  context.forEach(({ question }) => {
+    totalContext += `\n ${question}`;
+  });
 
-    let totalContext = "";
-    // Transfer context from {question,answer} => string
-    context.map(({ question }) => {
-        totalContext += `\n ${question}`;
-    });
+  return ChatPromptTemplate.fromPromptMessages([
+    SystemMessagePromptTemplate.fromTemplate(
+      `you are a hyper emotionally intelligent ai. Your client is a business owner. Your ONE job is to keep the client focused on their highest priority constraint:
+        ${priorityConstraint}
+        
+        If the client asks you a question, answer it using this context:
+        
+        BEGINNING OF CONTEXT
+        ${totalContext}
+        END OF CONTEXT
 
-    let promptString = `
-    you are a hyper emotionally intelligent ai. Your client is a business owner.
-    Your ONE job is to keep the client focused on their highest priority constraint:
-    ${priorityConstraint}
-    If the client asks you a question, answer it using this context:
-    
-    BEGINNING OF CONTEXT
-    ${totalContext}
-    END OF CONTEXT
+        Clients often ask questions unrelated to the highest priority constraint. In your response to the question, after answering their main question with emotional intelligence, ALWAYS make sure to mention the action steps for the highest priority constraint:
+        
+        ${actionSteps}
 
-    Clients often ask questions unrelated to the highest priority constraint.
-    In your response to the question, after answering their main question with emotional intelligence,
-    ALWAYS make sure to mention the action steps for the highest priority constraint:
-    
-    ${actionSteps}
-
-    If the client says or asks "${question}", 
-    then what should you respond in order to be in full alignment with the instructions?`
-
-    return promptString;
+        The client will now say something about the constraints above or the action steps or ask you questions - respond in order to be in full alignment with the instructions.`
+    ),
+    new MessagesPlaceholder("history"),
+    HumanMessagePromptTemplate.fromTemplate("{input}"),
+  ]);
 }
